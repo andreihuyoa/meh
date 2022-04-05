4df3693be8081dbb41119515b763dd210598e4a3
Ben-Sicat <benicat00@gmail.com>
2022-04-05 19:58:28 +0800

    testing

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { StyledEngineProvider } from '@mui/material/styles';
+import Navbar from './Navbar';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <StyledEngineProvider injectFirst>
+    <Navbar />
+  </StyledEngineProvider>,
+  document.querySelector("#root")
 );
 
 // If you want to start measuring performance in your app, pass a function
