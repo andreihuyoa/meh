203f6b749357726ed64871926e5e60fb2cbd77ac
Ben-Sicat <bensicat00@gmail.com>
2022-04-07 22:04:05 +0800

    added mediabar file

--- frontend_react/src/index.js
+++ frontend_react/src/index.js
@@ -4,17 +4,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 //import { theme } from "./theme"
-
-
 import {BrowserRouter} from 'react-router-dom';
 
-ReactDOM.render(<BrowserRouter>
-{/* <ThemeProvider theme ={theme}>
-    
-</ThemeProvider> */}
-<App />
-</BrowserRouter>,
-document.getElementById('root')
-);
+ReactDOM.render(<App />,document.getElementById('root'));
 
 
