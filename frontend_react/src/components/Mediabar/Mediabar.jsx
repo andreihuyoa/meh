e6c6994b341fd171121df7da9a55b74284ecfdd7
Sirben <93244935+Ben-Sicat@users.noreply.github.com>
2022-04-13 01:13:04 +0800

    Delete frontend_react/src directory

--- frontend_react/src/components/Mediabar/Mediabar.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import classes from './Mediabar.module.css';
-import {Button} from '@material-ui/core'
-const Mediabar = () => {
-  return (
-   <div>MediaBar</div>
-  )
-}
-export default Mediabar
